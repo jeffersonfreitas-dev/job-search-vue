e997dea7e2768773bb52fde698cd6102f6b3fa37
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-02-07 21:00:02 -0300

    The hero component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full text-sm">
+  <header :class="['w-full', 'text-sm', headerHeightClass]">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
@@ -44,6 +44,14 @@ export default {
         "Jobs",
       ],
       isLoggedIn: false
+    }
+  },
+  computed: {
+    headerHeightClass() {
+      return {
+        "h-16": !this.isLoggedIn,
+        "h-32": this.isLoggedIn
+      }
     }
   },
   methods: {
---
+++ src/components/TheHeadline.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter"><span :class="actionClasses">{{ action }} </span> 
+      <br />
+      for everyone</h1>
+    <h2 class="text-3xl font-light">Find your next job at Jeff Corp</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheHeadline",
+  data () {
+    return {
+      action: "Build",
+      interval: null
+    }
+  },
+  created() {
+    this.changeTitle();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+  },
+  computed: {
+    actionClasses() {
+      return {
+        [this.action.toLowerCase()]: true
+      }
+    }
+  },
+  methods: {
+    changeTitle(){
+      this.interval = setInterval(() => {
+        const actions = ["Build", "Create", "Desing", "Code"];
+        const currentActionIndex = actions.indexOf(this.action);
+        const nextActionIndex = (currentActionIndex + 1 ) % 4;
+        const ne3xAction = actions[nextActionIndex];
+        this.action = ne3xAction;
+      }, 2000)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+.create {
+  color: #34a853;
+}
+.desing {
+  color: #f9ab00;
+}
+.code {
+  color: #d93025;
+}
+</style>
