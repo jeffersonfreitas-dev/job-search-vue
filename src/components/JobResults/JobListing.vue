04c1fc5794b13169d5ef4b5843468ef96b5e2d4c
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-02-16 21:02:25 -0300

    Axios request to json-server mock

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,17 +1,19 @@
 <template>
   <li class="mb-7">
-      <router-link to="/jobs/results/1" class="mx-auto block rounded border border-solid 
+      <router-link :to="jobPageLink" class="mx-auto block rounded border border-solid 
         border-brand-gray-2 bg-white hover:shadow-gray">
           <div class="mx-8 border-b border-solid border-brand-gray-2 pt-5 pb-2">
-            <h2 class="mb-2 text-2xl">Technical Program Manager, Perception, Augmented Reality</h2>
+            <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
           
 
           <div class="flex flex-row align-middle">
             <div class="mr-5">
-              <span>Jeff</span>
+              <span>{{ job.organization }}</span>
             </div>
             <div>
-              <span>Iguatu, CE, BR</span>
+              <ul>
+                <li v-for="location in job.locations" :key="location" class="mr-5 inline-block"><span>{{ location }}</span></li>
+              </ul>
             </div>
           </div>
         </div>
@@ -21,19 +23,14 @@
             <h3 class="mt-1 mb-2">Qualifications:</h3>
             <div>
               <ul class="list-disc pl-8">
-                <li>Bacelor's degree or equivalent practical experience.</li>
-                <li>5 years of experience in program management.</li>
-                <li>
-                  Experience analyzing data through querying database (e.g.,SQL) using spreadsheet software,
-                    and creating statistical models.
-                </li>
+                <li v-for="qualification in job.minimumQualifications" :key="qualification">{{ qualification }}</li>
               </ul>
             </div>
           </div>
         </div>
 
         <div class="mt-2 text-center">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1">Expand</router-link>
+          <router-link :to="jobPageLink" class="text-brand-blue-1">Expand</router-link>
         </div>
       </router-link>
   </li>
@@ -41,6 +38,17 @@
 
 <script>
 export default {
-  name: "JobListing"
-}
+  name: "JobListing",
+  props: {
+    job: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`;
+    }
+  }
+};
 </script>
