9b73fea991ee876b6cacdb8fdc75c339bf772af5
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-02-23 20:46:13 -0300

    Pagination JobListings

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,8 +1,26 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <job-listing v-for="job in jobs" :key="job.id" :job="job"/>
+      <job-listing v-for="job in displayJobs" :key="job.id" :job="job"/>
     </ol>
+    <div class="mx-auto mt-8">
+      <div class="flex flex-row flex-nowrap">
+        <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+
+        <div class="flex items-center justify-center">
+          <router-link v-if="previousPage" :to="{name: 'JobResults', query: {page: previousPage}}" role="link"
+          class="mx-3 text-sm font-semibold text-brand-blue-1">
+            Previous
+          </router-link>
+
+          <router-link v-if="nextPage" :to="{name: 'JobResults', query: {page: nextPage}}" role="link"
+          class="mx-3 text-sm font-semibold text-brand-blue-1">
+            Next
+          </router-link>
+        </div>
+
+      </div>
+    </div>
   </main>
 </template>
 
@@ -20,8 +38,30 @@ import JobListing from '@/components/JobResults/JobListing.vue';
         jobs: []
       }
     },
+    computed: {
+      currentPage() {
+        return Number.parseInt(this.$route.query.page || "1");
+      }, 
+      previousPage() {
+        const previousPage = this.currentPage - 1;
+        const firstPage = 1;
+        return previousPage >= firstPage ? previousPage : undefined;
+      },
+      nextPage() {
+        const nextPage = this.currentPage + 1;
+        const maxPage = Math.ceil(this.jobs.length / 10);
+        return nextPage <= maxPage ? nextPage : undefined;
+      },  
+      displayJobs() {
+        const pageNumber = this.currentPage;
+        const firstJobIndex = (pageNumber - 1) * 10;
+        const lastJobIndex = pageNumber * 10;
+        return this.jobs.slice(firstJobIndex, lastJobIndex);
+      }
+    },
     async mounted() {
-      const response = await axios.get("http://localhost:3000/jobs");
+      const baseUrl = import.meta.env.VITE_APP_API_URL;
+      const response = await axios.get(`${baseUrl}/jobs`);
       this.jobs = response.data;
     }
   }
--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,10 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  scrollBehavior(){
+    return {top: 0, left: 0, behavior: "smooth"};
+  }
+
 });
 
 export default router;
